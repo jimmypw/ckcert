695d93b57d803380f43fc6970da7dcb5c3000d8c
James Park-Watt <jparkwatt@ukcloud.com>
2019-03-04 12:47:26 +0000

    Made the application more dynamic
    You are able to specify the remaining time to check for either in days or in a percent of the total lifespan of the certificate.

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ The exit status
 * exitRENEW   = 1
 * exitEXPIRED = 2
 * exitERROR   = 3
+
+## Usage
+
+### To check minimum remaining days
+
+`./ckcert -f "path to certificate" -d "minimum days"`
+
+e.g.
+
+`./ckcert -f /tmp/cert.crt -d 100`
+
+This would make sure there are at least 100 days remaining on the certificate
+
+### To check minimum remaining percentage
+
+`./ckcert -f "path to certificate" -p "percentage of validity"`
+
+e.g. 
+
+`./ckcert -f /tmp/cert.crt -p 30`
+
+This will ensure that there is at least 30% of the life of the certificate remaining.
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,41 +13,144 @@ import (
 const (
 	exitOK      = 0
 	exitRENEW   = 1
-	exitEXPIRED = 2
-	exitERROR   = 3
+	exitERROR   = 2
+	modeDAYS    = 3
+	modePERCENT = 4
 )
 
-func main() {
-	currenttime := time.Now()
-	if len(os.Args) < 2 {
-		fmt.Println("No input file specified")
+// Config struct will hold command line switch configuration.
+type Config struct {
+	mode int
+	days *int
+	prct *int
+	file *string
+}
+
+func isFlagSet(name string) bool {
+	isset := false
+	flag.Visit(func(thisflag *flag.Flag) {
+		if thisflag.Name == name {
+			isset = true
+		}
+	})
+
+	return isset
+}
+
+func processFlags() *Config {
+	conf := new(Config)
+	conf.file = flag.String("f", "", "The certificate file")
+	conf.days = flag.Int("d", 0, "Number of valid days to check for on certificate")
+	conf.prct = flag.Int("p", 0, "Percent of relative validity remaining on the certificate")
+	flag.Parse()
+
+	if !isFlagSet("f") {
+		fmt.Println("No input file (-f) specified")
 		os.Exit(exitERROR)
 	}
-	file, err := ioutil.ReadFile(os.Args[1])
+
+	if isFlagSet("d") && isFlagSet("p") {
+		fmt.Println("Switches -p and -d are mutrally exclusive")
+		os.Exit(exitERROR)
+	}
+
+	if (!isFlagSet("d")) && (!isFlagSet("p")) {
+		fmt.Println("Either -p or -d must be specified")
+		os.Exit(exitERROR)
+	}
+
+	if isFlagSet("d") {
+		if *conf.days < 1 {
+			fmt.Println("-d must be a positive integer")
+			os.Exit(exitERROR)
+		}
+		conf.mode = modeDAYS
+	} else {
+		if (*conf.prct < 1) || (*conf.prct > 100) {
+			fmt.Println("-p must be a positive integer between 1 and 100")
+			os.Exit(exitERROR)
+		}
+		conf.mode = modePERCENT
+	}
+
+	return conf
+}
+
+func readAndParseCertificate(certpath string) *x509.Certificate {
+	file, err := ioutil.ReadFile(certpath)
 	if err != nil {
 		fmt.Println("ERROR: Unable to read certificate.")
 		os.Exit(exitERROR)
 	}
-	decoded, rest := pem.Decode(file)
-	_ = rest
+	decoded, _ := pem.Decode(file)
 	parsedcertificate, err := x509.ParseCertificate(decoded.Bytes)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(exitERROR)
 	}
-	validity := parsedcertificate.NotAfter.Sub(parsedcertificate.NotBefore)
-	remaining := parsedcertificate.NotAfter.Sub(currenttime)
+	return parsedcertificate
+}
 
-	if remaining.Hours() < 0 {
-		fmt.Println("The certificate has expired.")
-		os.Exit(exitEXPIRED)
-	}
+func hoursToDays(hours float64) float64 {
+	return hours / 24.0
+}
 
-	if remaining.Hours() < (validity.Hours() / 2) {
-		fmt.Println("The certificate is approaching expiry.")
-		os.Exit(exitRENEW)
-	}
-
+func certExpiring() {
+	fmt.Println("The certificate is approaching expiry.")
+	os.Exit(exitRENEW)
+}
+func certOk() {
 	fmt.Println("The certificate is OK!")
 	os.Exit(exitOK)
+}
+
+func daysRemaining(begin, end time.Time, minimumdays int) bool {
+	returndata := true
+	delta := end.Sub(begin)
+	if hoursToDays(delta.Hours()) < float64(minimumdays) {
+		// Certificate is exiring
+		returndata = true
+	} else {
+		// Certificate is not expiring
+		returndata = false
+	}
+
+	return returndata
+}
+
+func percentRemaining(thetime time.Time, cert *x509.Certificate, percent int) bool {
+	returndata := true
+	validity := cert.NotAfter.Sub(cert.NotBefore)
+	minimumvalidity := (validity.Hours() / 100.0) * float64(percent)
+	deltaremaining := cert.NotAfter.Sub(thetime)
+	if deltaremaining.Hours() < minimumvalidity {
+		// Certificate is exiring
+		returndata = true
+	} else {
+		// Certificate is not expiring
+		returndata = false
+	}
+	return returndata
+}
+
+func main() {
+	config := processFlags()
+	cert := readAndParseCertificate(*config.file)
+
+	currenttime := time.Now()
+
+	switch config.mode {
+	case modeDAYS:
+		if daysRemaining(currenttime, cert.NotAfter, *config.days) {
+			certExpiring()
+		} else {
+			certOk()
+		}
+	case modePERCENT:
+		if percentRemaining(currenttime, cert, *config.prct) {
+			certExpiring()
+		} else {
+			certOk()
+		}
+	}
 }
